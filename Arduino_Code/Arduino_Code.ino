bb936763f523772a1a67ee16a663ab4347d1f240
Angel Lopez <angelarmandol@hotmail.com>
2019-04-12 20:06:16 -0700

    add processing interface

--- Arduino_Code/Arduino_Code.ino
+++ Arduino_Code/Arduino_Code.ino
@@ -1,38 +1,70 @@
-#include <AFMotor.h>                 //Include library AF Motor
-AF_DCMotor motor1(1);                // Motor 1 Izquierdo
-AF_DCMotor motor1(2);                // Motor 2 Derecho
-unsigned char velocidade1 = 0x00;    // Storage motor1 Speed (8 bits)
-
-void setup()
-{
-}
-
-
-void loop()
-{
-
-
-preformMove();
-
-
-  velocidade1 = 0xFF;
-  motor1.setSpeed(velocidade1);  // Set full Speed
-  motor1.run(FORWARD);           // Clockwise
-  delay(2000);                   // Wait 2 seconds
-
+// Includes the Servo library
+#include <Servo.h>. 
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 10;
+const int echoPin = 11;
+// Variables for the duration and the distance
+long duration;
+int distance;
+int anterior;
+int promedio;
+Servo myServo; // Creates a servo object for controlling the servo motor
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600);
+  myServo.attach(12); // Defines on which pin is the servo motor attached
 
 }
-
-
-public char irDirection(){
-//switch 
-
-	return direcction
+void loop() {
+  // rotates the servo motor from 15 to 165 degrees
+  for(int i=15;i<=165;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();// Calls a function for calculating the distance measured by the Ultrasonic sensor for each degree
+  
+  Serial.print(i); // Sends the current degree into the Serial Port
+  Serial.print(","); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  Serial.print(distance); // Sends the distance value into the Serial Port
+  Serial.print("."); // Sends addition character right next to the previous value needed later in the Processing IDE for indexing
+  }
+  // Repeats the previous lines from 165 to 15 degrees
+  for(int i=165;i>15;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
 }
+// Function for calculating the distance measured by the Ultrasonic sensor
+int calculateDistance(){ 
+  
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(1);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); // Reads the echoPin, returns the sound wave travel time in microseconds
+  distance= duration*0.034/2;
+   
+  int aux=distance;
+  promedio=(distance*anterior)/2;
+  if(distance>promedio){
+ distance=(distance+anterior);   
+  }
+ else{
+distance=anterior;
+ }
 
-public void preformMove(char d, int grados){
-
-
+ if (distance<10 || distance >4000){
+  distance=anterior;
+ }
+ anterior=aux;
+  return distance;
+  
 }
-
-
